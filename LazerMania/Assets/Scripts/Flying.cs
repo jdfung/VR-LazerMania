e9a8fbad394a9e356a9b6805b1fb0fe43bb9466b
jdfung <fungjiedeng1027@gmail.com>
2023-01-30 00:31:01 +0800

    initial commit

---
+++ LazerMania/Assets/Scripts/Flying.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Flying : MonoBehaviour
+{
+    public GameObject head;
+    public float flyingSpeed = 0.2f;
+    public bool isFlying = false;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        FlyIfFlying();
+    }
+
+    private void FlyIfFlying()
+    {
+        if (isFlying)
+        {
+            Vector3 flyDirection = Camera.main.transform.forward;
+            transform.position += flyDirection.normalized * flyingSpeed;
+        }
+    }
+}
---
+++ LazerMania/Assets/Scripts/RayCastGun.cs
@@ -0,0 +1,64 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RayCastGun : MonoBehaviour
+{
+    public Camera playerCamera;
+    public Transform laserOrigin;
+    public float gunRange = 50f;
+    public float fireRate = 0.2f;
+    public float laserDuration = 0.05f;
+    private RaycastHit hit;
+    LineRenderer laserLine;
+    float fireTimer;
+
+    void Awake()
+    {
+        laserLine = GetComponent<LineRenderer>();
+    }
+
+    void Update()
+    {
+        fireTimer += Time.deltaTime;
+        Vector3 rayOrigin = playerCamera.ViewportToWorldPoint(new Vector3(1f, 1f, 0));
+        if (Physics.Raycast(rayOrigin, playerCamera.transform.forward, out hit, gunRange))
+        {
+            //Debug.Log(hit.transform.tag);
+            if (hit.transform.CompareTag("Enemy") && fireTimer > fireRate)
+            {
+                fireTimer = 0;
+                laserLine.SetPosition(0, laserOrigin.position);
+                
+                if (Physics.Raycast(rayOrigin, playerCamera.transform.forward, out hit, gunRange))
+                {
+                    laserLine.SetPosition(1, hit.point);
+                    StartCoroutine(waiter(hit));
+                }
+                else
+                {
+                    laserLine.SetPosition(1, rayOrigin + (playerCamera.transform.forward * gunRange));
+                }
+                StartCoroutine(ShootLaser());
+            }
+        }
+    }
+
+    IEnumerator ShootLaser()
+    {
+        laserLine.enabled = true;
+        yield return new WaitForSeconds(laserDuration);
+        laserLine.enabled = false;
+
+    }
+
+
+    IEnumerator waiter(RaycastHit hit)
+    {
+        hit.transform.gameObject.SetActive(false);
+        //Wait for 2 seconds
+        yield return new WaitForSecondsRealtime(2);
+        hit.transform.gameObject.SetActive(true);
+
+    }
+}
